dc01e48de6385e1dbaca3c692c4fc273a33b7b12
Alec <alec.jordan.cs@gmail.com>
2019-05-22 22:51:05 -0400

    prettify react component

--- todo/src/components/TodoListContainer.js
+++ todo/src/components/TodoListContainer.js
@@ -1,56 +1,72 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../App.css';
-import { connect } from 'react-redux';
-import { addTodo, toggleTodo, deleteTodo } from '../actions';
+import {connect} from 'react-redux';
+import {addTodo, toggleTodo, deleteTodo} from '../actions';
 
 class TodoListContainer extends Component {
   constructor() {
     super();
     this.state = {
-      todo: ''
-    }
+      todo: '',
+    };
   }
 
-
-  handleChange = (e) => {
+  handleChange = e => {
     this.setState({[e.target.name]: e.target.value});
-  }
+  };
 
-  addTodoHandler = (e) => {
+  addTodoHandler = e => {
     e.preventDefault();
     const text = this.state.todo;
     const newTodo = {
       text,
       completed: false,
-      id: this.props.todos ? this.props.todos.length + text: 0 + text
-    }
+      id: this.props.todos ? this.props.todos.length + text : 0 + text,
+    };
     this.props.addTodo(newTodo);
-    this.setState({todo: ''})
-  }
+    this.setState({todo: ''});
+  };
 
-  toggleComplete = (todoId) => {
+  toggleComplete = todoId => {
     this.props.toggleTodo(todoId);
-  }
+  };
 
-  deleteTodo = (todoId) => {
+  deleteTodo = todoId => {
     this.props.deleteTodo(todoId);
-  }
-
+  };
 
   render() {
     return (
       <div className="TodoList-Container">
-        <ul>{this.props.todos.map(todo => {
-          return <div><li onClick={() => this.toggleComplete(todo.id)}
-          style={todo.completed ? {color: 'red', textDecoration: 'line-through'}: null}
-          key={todo.id}>{todo.text}</li>
-          <button type="button" onClick={() => this.deleteTodo(todo.id)}>X</button>
-          </div>
-        })}</ul>
+        <ul>
+          {this.props.todos.map(todo => {
+            return (
+              <div>
+                <li
+                  onClick={() => this.toggleComplete(todo.id)}
+                  style={
+                    todo.completed
+                      ? {color: 'red', textDecoration: 'line-through'}
+                      : null
+                  }
+                  key={todo.id}>
+                  {todo.text}
+                </li>
+                <button type="button" onClick={() => this.deleteTodo(todo.id)}>
+                  X
+                </button>
+              </div>
+            );
+          })}
+        </ul>
         <form onSubmit={this.addTodoHandler}>
-          <input type="text" name="todo" placeholder="todo..."
-          onChange={this.handleChange}
-          value={this.state.todo}/>
+          <input
+            type="text"
+            name="todo"
+            placeholder="todo..."
+            onChange={this.handleChange}
+            value={this.state.todo}
+          />
           <button type="Submit">Submit</button>
         </form>
       </div>
@@ -60,8 +76,11 @@ class TodoListContainer extends Component {
 
 const mapStateToProps = state => {
   return {
-    todos: state.todos
+    todos: state.todos,
   };
 };
 
-export default connect(mapStateToProps, { addTodo, toggleTodo, deleteTodo })(TodoListContainer);
+export default connect(
+  mapStateToProps,
+  {addTodo, toggleTodo, deleteTodo},
+)(TodoListContainer);
