1b5e9dd6df2a9c402df781b5dc27c1f1c05978c5
Alec <alec.jordan.cs@gmail.com>
2019-05-29 11:49:49 -0400

    fix missing return statement

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -11,9 +11,9 @@ const todoApp = (state = todos, action) => {
       return [...state, action.payload];
     case TOGGLE_TODO:
       return state.map(todo => {
-        todo.id === action.payload
+        return (todo.id === action.payload
           ? {...todo, completed: !todo.completed}
-          : todo;
+          : todo);
       });
 
     case DELETE_TODO:
